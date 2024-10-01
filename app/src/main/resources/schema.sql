857e9662246ba27a2062204afd6e70626dc417b5
Владислав <megoboom17@gmail.com>
2024-10-01 19:30:23 +0300

    fixe schema

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -2,24 +2,21 @@ DROP TABLE IF EXISTS url_checks;
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR NOT NULL,
-    created_at TIMESTAMP,
-    CONSTRAINT pk_url PRIMARY KEY (id)
+      id SERIAL PRIMARY KEY NOT NULL,
+      name VARCHAR NOT NULL,
+      created_at TIMESTAMP
 );
 
-CREATE TABLE url_checks
-(
+CREATE TABLE url_checks (
     id SERIAL PRIMARY KEY NOT NULL,
     url_id BIGINT REFERENCES urls (id),
     status_code INT,
     h1 VARCHAR,
     title VARCHAR,
     description TEXT,
-    created_at TIMESTAMP,
-    CONSTRAINT pk_url_checks PRIMARY KEY (id)
-
+    created_at TIMESTAMP
 );
+
 
 
 
